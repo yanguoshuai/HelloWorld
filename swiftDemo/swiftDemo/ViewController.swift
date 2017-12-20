03bc87fa7ea7bbefb322ff0b5bc5a4d6d66ac8d9
ygs <85275043@qq.com>
2017-12-20 14:17:25 +0800

    删除代码

--- swiftDemo/swiftDemo/ViewController.swift
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var tableView:UITableView
+
         
         // Do any additional setup after loading the view, typically from a nib.
     }
