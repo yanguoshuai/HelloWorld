0172cda76b09b7a380b1dd44307bc006572fb474
ygs <85275043@qq.com>
2017-12-20 14:11:02 +0800

    init commit

---
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by yangs on 2017/10/16.
+//  Copyright © 2017年 zhongyingyiliao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var tableView:UITableView
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
